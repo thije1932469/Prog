e0580851e0cd97eebff304c8a17c4a7c98ead7d3
thije1932469 <54541211+thije1932469@users.noreply.github.com>
2019-10-15 15:24:40 -0400

    exercise tab et boucle 1

--- atelier/Program.cs
+++ atelier/Program.cs
@@ -4,135 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Atelier_Boucle_Tableaux_exercise_2
+namespace AtelierBoucle_tableau
 {
     class Program
     {
-        static void AfficherMenu()
-        {
-            Console.WriteLine("Que voulez-vous faire?");
-            Console.WriteLine("1- Trouvez le plus grand nombre");
-            Console.WriteLine("2- Trouvez le plus petit nombre");
-            Console.WriteLine("3- Verifier si le nombre saisi existe dans le tableau");
-            Console.WriteLine("4- Faire la moyenne");
-            Console.WriteLine("5- Quitter le programme");
-        }
-        static void GrandNb(ref int[] tab)
-        {           
-            int plusGrandNb = 0;
-            
-            for (int i = 0; i < tab.Length; i++)
-            {
-                
-                if (plusGrandNb < tab[i])
-                {
-                    plusGrandNb = tab[i];
-                }
-            }
-            Console.WriteLine("Le plus grand nombre est : " + plusGrandNb);
-        }
-        static void PetitNombre(ref int[] tab)
-        {
-            int PetitNombre = 10000;
-
-            for (int i = 0; i < tab.Length; i++)
-            {
-                if (PetitNombre > tab[i])
-                {
-                   PetitNombre = tab[i];
-                }
-            }
-            Console.WriteLine("Le plus petit nombre est : " + PetitNombre);
-        }
-        static void Verification(ref int[] tab)
-        {
-            int nombreSaisie = 0;
-            int i = 0;
-            bool nombreSaisieExiste = false;
-            Console.WriteLine("Veuillez saisir un nombre");
-            nombreSaisie = Convert.ToInt32(Console.ReadLine());
-            while (nombreSaisieExiste = false && i < tab.Length)
-            {
-                if (tab[i] == nombreSaisie)
-                {
-                    nombreSaisieExiste = true;
-                   
-                }
-                else
-                {
-                    nombreSaisieExiste = false;
-                  
-                }
-               
-            }
-            if (nombreSaisieExiste = true)
-            {
-                Console.WriteLine("Le nombre existe.");
-            }
-            else
-            {
-                Console.WriteLine("Le nombre n'existe pas.");
-            }
-        }
-        static void Moyenne(ref int[] tab)
-        {
-            int moyenne = 0;
-            int total = 0;
-            for(int i = 0; i < tab.Length;i++)
-            {
-                total += tab[i];
-            }
-            
-            moyenne =  total / 300;
-            Console.WriteLine("La moyenne est de " + moyenne);
-                         
-        }
-       
-
-
         static void Main(string[] args)
         {
-            int nombre = 0;
-            int[] TabValeur = new int[300];
-            int plusGrandNombre = 0;
-            int plusPetitNombre = TabValeur[0];
-            bool valeurSaisie = false;
-            double moyenne = 0;
-            int choix = 0;
-
-            Random generateur = new Random();
-
-            Console.WriteLine("Ce programme va créer un tableau de 300 valeurs avec des nombres aléatoires");
-            for (int i = 0; i < TabValeur.Length; i++)
+            int NombreSaisi = 0;
+            Console.WriteLine("Ce programme vous donnera la factorielle d'un nombre de votre choix");
+            Console.WriteLine("Enter un nombre");
+            NombreSaisi = Convert.ToInt32(Console.ReadLine());
+            int ì = 0;
+            int total = 1;
+            for (int i = 1; i <= NombreSaisi;i++)
             {
-                TabValeur[i] = generateur.Next(1,10001);
-                
-
+                total = total * i;
             }
-
-            AfficherMenu();
-            choix = Convert.ToInt32(Console.ReadLine());
-            
-           
-            switch(choix)
-            {
-                case 1:
-                    GrandNb(ref TabValeur);                  
-                    break;
-                case 2:
-                    PetitNombre(ref TabValeur);
-                    break;
-                case 3:
-                    Verification(ref TabValeur);
-                    break;
-                case 4:
-                    Moyenne(ref TabValeur);
-                    break;
-                case 5:
-                    Environment.Exit(0);
-                    break;
-            }
-
+            Console.WriteLine("La factorielle est de " + total);
+            Console.WriteLine("Etes-vous satisfait monseigneur de vinci du premier?");
         }
     }
 }
